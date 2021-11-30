df740c5635fd88fb5af32f5c1511b34a11fd6aa1
MasterDan <MihaylovDS@gmail.com>
2021-11-30 19:45:40 +0300

    new

--- src/colorRgb.rs
+++ src/colorRgb.rs
@@ -3,3 +3,19 @@ struct ColorRgb {
     green: u8,
     blue: u8,
 }
+
+impl for ColorRgb {
+    fn new(r:u8,g:u8,b:u8) -> Self {
+        ColorRgb {
+            red: r,
+            green: g,
+            blue: b
+        }
+    }
+}
+
+impl fmt::Display for ColorRgb {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt{
+        
+    }
+}
