b2299dedf8a2a10580e9766803c5ec7e8362fcb3
MasterDan <MihaylovDS@gmail.com>
2021-12-03 19:54:10 +0300

    format for ppm file

--- src/color_rgb.rs
+++ src/color_rgb.rs
@@ -1,11 +1,14 @@
 use std::fmt::{Display, Error, Formatter};
 use std::result::Result;
 
-pub struct ColorRgb {
+#[derive(Clone, Copy)]
+pub(crate) struct ColorRgb {
     red: u8,
     green: u8,
     blue: u8,
 }
+
+pub(crate) struct ColorRgbPpm(ColorRgb);
 
 impl ColorRgb {
     pub fn new(r: u8, g: u8, b: u8) -> Self {
@@ -15,10 +18,18 @@ impl ColorRgb {
             blue: b,
         }
     }
+    pub fn to_ppm_format(self) -> ColorRgbPpm {
+        ColorRgbPpm(self)
+    }
 }
 
 impl Display for ColorRgb {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        write!(f, "{} {} {}", self.red, self.green, self.blue)
+        write!(f, "{}, {}, {}", self.red, self.green, self.blue)
+    }
+}
+impl Display for ColorRgbPpm {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        write!(f, "{} {} {}", self.0.red, self.0.green, self.0.blue)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,9 @@ mod color_rgb;
 
 fn main() {
     let color = ColorRgb::new(255, 255, 0);
-    println!("Hello, world! ({})", color);
+    println!(
+        "Color is ({})\nColor in .ppm is {}",
+        color,
+        color.to_ppm_format()
+    );
 }
