6c6810c909fc481cfa31fbfdf4c98784d06e37a4
MasterDan <MihaylovDS@gmail.com>
2021-12-12 17:05:16 +0300

    relative coords

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,19 @@ mod ppm_file;
 
 fn main() -> Result<(), Error> {
     let path = "image.ppm";
-    let mut output = File::create(path)?;
-    let image = PpmImage::new(256, 256, |row, column| {
+    let mut image_file = File::create(path)?;
+    let width = 512;
+    let height = 512;
+    print!("Generating Image \n");
+    let image = PpmImage::new(height, width, |row, column| {
         ColorRgb::new(
-            column.try_into().unwrap(),
-            (255 - row).try_into().unwrap(),
+            ((column as f32 / (width as f32 - 1_f32)) * 255_f32) as u8,
+            (((height as f32 - row as f32) / (height as f32 - 1_f32)) * 255_f32) as u8,
             (255.0 * 0.25) as u8,
         )
     });
-    write!(output, "{}", image)?;
+    print!("Saving Image to ppm file\n");
+    write!(image_file, "{}", image)?;
+    print!("Done!");
     Ok(())
 }
