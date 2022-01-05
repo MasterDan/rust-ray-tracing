b9961efcfb83ee88ba1a04433d22a54d2132ed9f
MasterDan <MihaylovDS@gmail.com>
2022-01-05 16:43:42 +0300

    deref for list

--- src/hittable/hittable_list.rs
+++ src/hittable/hittable_list.rs
@@ -1,6 +1,8 @@
 use crate::hittable::hit_record::HitRecord;
 use crate::hittable::hittable::Hittable;
 use crate::Ray;
+use core::ops::Deref;
+use core::ops::DerefMut;
 
 pub(crate) struct HittableList(Vec<Box<dyn Hittable>>);
 
@@ -14,7 +16,7 @@ impl HittableList {
     }
 
     pub fn add<T: Hittable + 'static>(&mut self, item: T) {
-        self.0.push(Box::new(item));
+        self.push(Box::new(item));
     }
 }
 
@@ -24,7 +26,7 @@ impl Hittable for HittableList {
         let mut hit_anything = false;
         let mut closest_so_far = t_max;
 
-        for object in self.0.iter() {
+        for object in self.iter() {
             if object.hit(r, t_min, closest_so_far, rec) {
                 hit_anything = true;
                 closest_so_far = temp_rec.t;
@@ -32,5 +34,18 @@ impl Hittable for HittableList {
             }
         }
         hit_anything
+    }
+}
+
+impl Deref for HittableList {
+    type Target = Vec<Box<dyn Hittable>>;
+    fn deref(&self) -> &Vec<Box<dyn Hittable>> {
+        &self.0
+    }
+}
+
+impl DerefMut for HittableList {
+    fn deref_mut(&mut self) -> &mut Vec<Box<dyn Hittable>> {
+        &mut self.0
     }
 }
