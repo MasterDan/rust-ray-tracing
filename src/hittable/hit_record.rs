63a0acaedf8a2d4a6dcfe7b1cf340fb2bfbb4cf1
MasterDan <MihaylovDS@gmail.com>
2022-01-05 16:35:21 +0300

    hittable impl

--- src/hittable/hit_record.rs
+++ src/hittable/hit_record.rs
@@ -18,4 +18,20 @@ impl HitRecord {
             -outward_normal
         };
     }
+
+    pub fn empty() -> Self {
+        HitRecord {
+            p: Point3(Vec3::new(0.0, 0.0, 0.0)),
+            normal: Vec3::new(0.0, 0.0, 0.0),
+            t: 0.0,
+            front_face: false,
+        }
+    }
+
+    pub fn update(&mut self, other: &HitRecord) {
+        self.front_face = other.front_face;
+        self.p = other.p;
+        self.t = other.t;
+        self.normal = other.normal;
+    }
 }
--- src/hittable/hittable_list.rs
+++ src/hittable/hittable_list.rs
@@ -1,3 +1,36 @@
+use crate::hittable::hit_record::HitRecord;
 use crate::hittable::hittable::Hittable;
+use crate::Ray;
 
 pub(crate) struct HittableList(Vec<Box<dyn Hittable>>);
+
+impl HittableList {
+    pub fn clear(&mut self) {
+        self.0.clear();
+    }
+
+    pub fn new() -> Self {
+        HittableList(Vec::new())
+    }
+
+    pub fn add<T: Hittable + 'static>(&mut self, item: T) {
+        self.0.push(Box::new(item));
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool {
+        let temp_rec = HitRecord::empty();
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+
+        for object in self.0.iter() {
+            if object.hit(r, t_min, closest_so_far, rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec.update(&temp_rec);
+            }
+        }
+        hit_anything
+    }
+}
