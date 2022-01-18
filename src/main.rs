2c93774b1bba479e98d256df638d90fef720971d
MasterDan <MihaylovDS@gmail.com>
2022-01-18 21:07:57 +0300

    solved some errors

--- src/main.rs
+++ src/main.rs
@@ -51,12 +51,12 @@ fn main() -> Result<(), Error> {
     );
     print!("\nGenerating Image\n\n");
     let image = PpmImageLazy::new(height, width).calculate(|row, column| {
-        let mut pixel_color = Vec3::new(0.0, 0.0, 0.0);
         let mut rng = rand::thread_rng();
-        let v = 1.0 - (row as f64 + rng.gen::<f64>()) / (height - 1) as f64;
-        let u = (column as f64 + rng.gen::<f64>()) / (width - 1) as f64;
-        let ray = camera.get_ray(u, v);
+        let mut pixel_color = Vec3::new(0.0, 0.0, 0.0);
         for _ in 1..=samples_per_pixel {
+            let v = 1.0 - (row as f64 + rng.gen::<f64>()) / (height - 1) as f64;
+            let u = (column as f64 + rng.gen::<f64>()) / (width - 1) as f64;
+            let ray = camera.get_ray(u, v);
             pixel_color += ray.ray_color(&world, SETTINGS.max_depth)
         }
         let color = pixel_color.scale(samples_per_pixel).to_color_rgb_safe();
