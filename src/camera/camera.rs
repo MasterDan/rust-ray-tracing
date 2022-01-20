466f752fa90c910ad287481dfd422fc2d51ca0a7
MasterDan <MihaylovDS@gmail.com>
2022-01-20 13:03:06 +0300

    added position to camera

--- src/camera/camera.rs
+++ src/camera/camera.rs
@@ -12,30 +12,31 @@ pub(crate) struct Camera {
 }
 
 impl Camera {
-    pub fn new(v_f_o_v: f64) -> Self {
-        let theta = Degrees(v_f_o_v).toRadians();
+    pub fn new(lookfrom: Point3, lookat: Point3, vup: Vec3, v_f_o_v: f64) -> Self {
+        let theta = Degrees(v_f_o_v).to_radians();
         let h = (theta / 2.0).tan();
         let viewport_height = SETTINGS.viewport_height * h;
         let viewport_width = SETTINGS.aspect_ratio * viewport_height;
 
-        let origin = Vec3::new(0.0, 0.0, 0.0);
-        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-        let vertical = Vec3::new(0.0, viewport_height, 0.0);
+        let w = (lookfrom - lookat).unit();
+        let u = (Vec3::cross(vup, w)).unit();
+        let v = Vec3::cross(w, u);
+
+        let origin = lookfrom;
+        let horizontal = viewport_width * u;
+        let vertical = viewport_height * v;
         Camera {
             origin,
             horizontal,
             vertical,
-            lower_left_corner: origin
-                - horizontal / 2.0
-                - vertical / 2.0
-                - Vec3::new(0.0, 0.0, SETTINGS.focal_length),
+            lower_left_corner: origin - horizontal / 2.0 - vertical / 2.0 - w,
         }
     }
 
-    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+    pub fn get_ray(&self, s: f64, t: f64) -> Ray {
         Ray::new(
             self.origin,
-            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
+            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin,
         )
     }
 }
--- src/degrees/degrees.rs
+++ src/degrees/degrees.rs
@@ -3,7 +3,7 @@ use core::f64::consts::PI;
 pub(crate) struct Degrees(pub f64);
 
 impl Degrees {
-    pub fn toRadians(self) -> f64 {
+    pub fn to_radians(self) -> f64 {
         self.0 * PI / 180.0
     }
 }
